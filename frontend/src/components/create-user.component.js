aa3192090964782796afe239bb1dad2ae2433057
Tianrens <tshe695@aucklanduni.ac.nz>
2020-04-05 22:44:00 +1200

    Fixed typo

--- frontend/src/components/create-user.component.js
+++ frontend/src/components/create-user.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Homepage extends Component {
+export default class CreateUser extends Component {
     render() {
         return (
             <div>
