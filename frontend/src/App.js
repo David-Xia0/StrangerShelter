3cf1b56184faf407c7c2c2e9f89fbf6f4b6812eb
Tianrens <tshe695@aucklanduni.ac.nz>
2020-04-13 12:38:05 +1200

    Added Join component to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
-import Homepage from "./components/homepage.component"
-import CreateUser from "./components/create-user.component"
+import Homepage from "./components/homepage.component";
+import CreateUser from "./components/create-user.component";
+import Join from "./components/join.component";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <br/>
         <Route path="/" exact component={Homepage} />
         <Route path="/user" component={CreateUser} />
+        <Route path="/join" component={Join} />
       </div>
     </Router>
   );
