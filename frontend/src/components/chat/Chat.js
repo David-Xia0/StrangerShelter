236139b26cfdb75c309559e338d797b145998c8a
Tianrens <tshe695@aucklanduni.ac.nz>
2020-04-13 13:10:48 +1200

    Added package query-string to frontend

--- frontend/src/components/chat/Chat.js
+++ frontend/src/components/chat/Chat.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
 const Chat = () => {
+    useEffect(() => {
+        const data = queryString.parse(location.search);
+    })
     return(
         <h1>Chat</h1>
     )
