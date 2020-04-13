71954ad853895429d409c4481bfb3e49257745a8
Tianrens <tshe695@aucklanduni.ac.nz>
2020-04-13 12:21:55 +1200

    Edited server.js to use socket.io

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const socketio = require('socket.io');
+const http = require('http');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +27,6 @@ const usersRouter = require('./routes/users');
 
 app.use('/users', usersRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+server.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
 });
