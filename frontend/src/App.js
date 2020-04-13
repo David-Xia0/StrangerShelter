8588b63b76c0034ba4d4865e9b70ccc9b69d882c
Tianrens <tshe695@aucklanduni.ac.nz>
2020-04-13 15:42:23 +1200

    Added ; to end of some lines

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
-import Homepage from "./components/homepage.component"
-import CreateUser from "./components/create-user.component"
+import Homepage from "./components/homepage.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
