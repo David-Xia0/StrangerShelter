7662cb0cbebf44c7c661d50d2ddc30f5c775c5db
Tianrens <tshe695@aucklanduni.ac.nz>
2020-04-14 15:08:03 +1200

    Set up basic socketio

--- frontend/src/components/chatpage/ChatPage.js
+++ frontend/src/components/chatpage/ChatPage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
 
 import InfoBar from "./infobar/InfoBar";
 import MessagesBox from "./messagesbox/MessagesBox";
@@ -6,20 +8,48 @@ import MessageInput from "./messageinput/MessageInput";
 
 import './ChatPage.css';
 
-const ChatPage = () => {
+let socket;
+
+const ChatPage = ({ location }) => {
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = '';
+    const ENDPOINT = 'http://localhost:5000/';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+    
+        socket = io(ENDPOINT);
+    
+        setRoom(room);
+        setName(name)
+    
+        socket.emit('join', { name, room }, (error) => {
+          if(error) {
+            alert(error);
+          }
+        });
+      }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', message => {
+          setMessages(messages => [ ...messages, message ]);
+        });
+        
+        socket.on("roomData", ({ users }) => {
+          setUsers(users);
+        });
+    }, []);
 
     const sendMessage = (event) => {
         event.preventDefault();
 
-        console.log("sending " + message);
-        setMessage('');
+        if(message) {
+          socket.emit('sendMessage', message, () => setMessage(''));
+        }
     }
 
     return (
