fcd419a205aa9ed6b0c1a742ab176874c6f14da0
David Xiao <davidjinxiao@hotmail.com>
2020-04-26 13:47:54 +1200

    added rooms and puts the user ina differnet room if current room is full

--- backend/server.js
+++ backend/server.js
@@ -26,17 +26,28 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
+    var roomNumber = 1;
+    var r = getUsersInRoom(roomNumber);
+    console.log("here");
+    while (true) {
+      if (r.length > 1) {
+        roomNumber++;
+        r = getUsersInRoom(roomNumber);
+      } else {
+        break;
+      }
+    }
+    const { error, user } = addUser({ id: socket.id, name, room: roomNumber });
 
-    if(error) return callback(error);
+    if (error) return callback(error);
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'Admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.emit('message', { user: 'Admin', text: `${user.name}, welcome to room ${user.room}.` });
     socket.broadcast.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has joined!` });
-    console.log(user.name + "joined chat: "+ user.room);
+    console.log(user.name + "joined chat: " + user.room);
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     console.log(getUsersInRoom(user.room));
     callback();
@@ -50,13 +61,13 @@ io.on('connection', function(socket){
     callback();
   });
 
-  socket.on('disconnect', function(){
- 
+  socket.on('disconnect', function () {
+
     const user = removeUser(socket.id);
-    if(user) {
+    if (user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-      console.log("disconnected "+ user.name);
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+      console.log("disconnected " + user.name);
     }
 
   });
@@ -71,5 +82,5 @@ app.use('/Chats', chatsRouter);
 app.use('/Statistics', statisticsRouter);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}`);
 });
