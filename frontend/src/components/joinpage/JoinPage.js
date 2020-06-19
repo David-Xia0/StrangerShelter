de7fb3df8630d576cd84df1e10c31b6e6b31b3b2
Tianrens <tshe695@aucklanduni.ac.nz>
2020-06-19 19:59:24 +1200

    Join page bootstrap

--- frontend/src/components/joinpage/JoinPage.js
+++ frontend/src/components/joinpage/JoinPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import logo from '../../icons/shelterNoBackground.png';
+import {Container, Row, Col} from 'react-bootstrap';
 import { BrowserView, MobileView } from "react-device-detect";
 
 
@@ -27,39 +28,64 @@ export default function EnterChatRoom() {
 
 
   return (
-    <div className="joinOuterContainer">
-      <img className="logo" src={logo} />
-      <h1 className="title">Welcome to Stranger Shelter!</h1>
-      <h1 className="titleDescription"> come talk to new people and make new friends </h1>
-      <div className="joinHorizontalContainer">
-        <div className="joinInnerContainer">
-          <h1 className="heading">Please enter a display name below</h1>
-          <div>
-            <input placeholder="Name" className="joinInput mt-20" type="text" onChange={(event) => setName(event.target.value.trim())} />
-          </div>
-          <Link onClick={e =>
-            axios.post(ENDPOINT + "/users/add", { username: name, chatID: room }).then(res => console.log(res.data)).catch(err => alert("Servers Might be Down")
-            )
-          }
+    <Container>
+      <Row>
+       <img className="logo" src={logo} />
+       <h1 className="title">Welcome to Stranger Shelter!</h1>
+       <h3 className="titleDescription"> come talk to new people and make new friends </h3>
+      </Row>
+      <Row>
+        <h2 className="heading">Please enter a display name below</h2>
+      </Row>
+      <Row>
+        <Col>
+          <input placeholder="Name" className="joinInput mt-20" type="text" onChange={(event) => setName(event.target.value.trim())} />
+        </Col>
+        <Col>
+          <button className={'button mt-20'} type="submit">connect</button>
+        </Col>
+      </Row>
+      <Row>
+        <h1 className="rulesText"> By using our service you agree with to our terms of service and privacey policy</h1>
+      </Row>
+      <row>
+        
+      </row>
 
-            to={`/chat?name=${name}&room=${room}`}>
-            <button className={'button mt-20'} type="submit">connect</button>
-          </Link>
+    </Container>
+    // <div className="joinOuterContainer">
+    //   <img className="logo" src={logo} />
+    //   <h1 className="title">Welcome to Stranger Shelter!</h1>
+    //   <h1 className="titleDescription"> come talk to new people and make new friends </h1>
+    //   <div className="joinHorizontalContainer">
+    //     <div className="joinInnerContainer">
+    //       <h1 className="heading">Please enter a display name below</h1>
+    //       <div>
+    //         <input placeholder="Name" className="joinInput mt-20" type="text" onChange={(event) => setName(event.target.value.trim())} />
+    //       </div>
+    //       <Link onClick={e =>
+    //         axios.post(ENDPOINT + "/users/add", { username: name, chatID: room }).then(res => console.log(res.data)).catch(err => alert("Servers Might be Down")
+    //         )
+    //       }
 
-          <h1 className="rulesText"> By using our service you agree with to our terms of service and privacey policy</h1>
-          <Link to={`/TOS`} target="_blank" >
-            <h1 className="termsOfService"> TOS and PP</h1>
-          </Link>
-        </div>
-        <div className="rulesBox">
-          <h1 className="heading"> Rules:  </h1>
-          <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;No racism, homophobia, sexism.</li>
-          <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;No catfishing, scaming, phishing.</li>
-          <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;Please be nice and welcoming to everyone.</li>
-          <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;please choose an aporpriate name.</li>
-        </div>
-      </div>
-    </div>
+    //         to={`/chat?name=${name}&room=${room}`}>
+    //         <button className={'button mt-20'} type="submit">connect</button>
+    //       </Link>
+
+    //       <h1 className="rulesText"> By using our service you agree with to our terms of service and privacey policy</h1>
+    //       <Link to={`/TOS`} target="_blank" >
+    //         <h1 className="termsOfService"> TOS and PP</h1>
+    //       </Link>
+    //     </div>
+    //     <div className="rulesBox">
+    //       <h1 className="heading"> Rules:  </h1>
+    //       <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;No racism, homophobia, sexism.</li>
+    //       <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;No catfishing, scaming, phishing.</li>
+    //       <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;Please be nice and welcoming to everyone.</li>
+    //       <li className="rulesText"> &nbsp;&nbsp;&nbsp;&nbsp;please choose an aporpriate name.</li>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
