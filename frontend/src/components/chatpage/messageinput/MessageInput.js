aa6938b5aa7fdeb49d149b057227f0a7d314bc94
Tianrens <tshe695@aucklanduni.ac.nz>
2020-07-05 13:19:00 +1200

    Changed css

--- frontend/src/components/chatpage/messageinput/MessageInput.js
+++ frontend/src/components/chatpage/messageinput/MessageInput.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 import './MessageInput.css';
 
 const MessageInput = ({ setMessage, sendMessage, message }) => (
-  <form className="chatInputForm">
-    <input
+  <Row className='inputArea'>
+      <form className="chatInputForm">
+      <input
       className="chatFormInput"
       type="text"
       placeholder="Type a message..."
       value={message}
       onChange={({ target: { value } }) => setMessage(value)}
       onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-    />
+    />  
     <button className="formSendButton" onClick={e => sendMessage(e)}>Send</button>
   </form>
+  </Row>
+
+
 )
 
 
